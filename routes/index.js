3fa6dbb419dbb5fbd08d280af5ddffc0127dd2b6
Danil Nikiforov <danil.nikiphorov@gmail.com>
2014-07-01 17:33:21 +0700

    added button with redirect to auth/google page. link auth/google page with jade template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SmartTask' });
+});
+
+router.get('/auth/google', function(req, res) {
+  res.render('auth');
 });
 
 module.exports = router;
