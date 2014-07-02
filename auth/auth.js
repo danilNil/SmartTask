6ce6157b1531d01c722b7833d6d71cec5ffa5a61
Danil Nikiforov <danil.nikiphorov@gmail.com>
2014-07-02 15:51:48 +0700

    moved code related to auth to separated module

---
+++ auth/auth.js
@@ -0,0 +1,41 @@
+var open = require('open');
+var googleapis = require('googleapis'),
+OAuth2 = googleapis.auth.OAuth2;
+
+var oauth2Client =
+    new OAuth2(
+    	"181187044153-nn51k2mjdglll4b0ipi1s1o5n6rkbj9r.apps.googleusercontent.com",
+     	"9WIyr3z0N6anZZC9yKjspOQG", 
+     	"http://127.0.0.1:3000/oauth2callback"
+     	);
+
+   
+exports.getToken = function  (url, callback){
+  	var code = url.query.code;
+  	// request access token
+	oauth2Client.getToken(code, function(err, tokens) {
+	  // set tokens to the client
+	  // TODO: tokens should be set by OAuth2 client.
+	  oauth2Client.setCredentials(tokens);
+	  console.log(tokens);
+	  callback(err);
+	});
+}
+
+
+exports.onLogin = function (callback) {
+	// generates a url that allows offline access and asks permissions
+	// for tasks scope.
+	var scopes = [
+	  'https://www.googleapis.com/auth/tasks'	  
+	];
+
+	var url = oauth2Client.generateAuthUrl({
+	  access_type: 'offline',
+	  scope: scopes.join(" ") // space delimited string of scopes
+	});
+
+	open(url, function (err) {
+  		callback(err);
+	});
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var router = express.Router();
-var readline = require('readline');
-
+var router = express.Router();	
+var url = require("url");
+var auth = require('../auth/auth.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,47 +9,18 @@ router.get('/', function(req, res) {
 });
 
 router.get('/oauth2callback', function(req, res) {
-  console.log(res);
+  	//console.log(res);
+  	debugger;
+  	var parsedUrl = url.parse(req.url, true);
+  	auth.getToken(parsedUrl, function (err) {
+	  	res.render('auth');
+  	});
 });
 
 router.get('/auth/google', function(req, res) {
-	var googleapis = require('googleapis'),
-    OAuth2 = googleapis.auth.OAuth2;
-
-	var oauth2Client =
-	    new OAuth2(
-	    	"181187044153-nn51k2mjdglll4b0ipi1s1o5n6rkbj9r.apps.googleusercontent.com",
-	     	"9WIyr3z0N6anZZC9yKjspOQG", 
-	     	"http://127.0.0.1:3000/oauth2callback"
-	     	);
-
-	// generates a url that allows offline access and asks permissions
-	// for Google+ scope.
-	var scopes = [
-	  'https://www.googleapis.com/auth/tasks'	  
-	];
-
-	var url = oauth2Client.generateAuthUrl({
-	  access_type: 'offline',
-	  scope: scopes.join(" ") // space delimited string of scopes
-	});
-
-	var rl = readline.createInterface({
-	  input: process.stdin,
-	  output: process.stdout
-	});
-	console.log('Visit the url: ', url);
-	rl.question('Enter the code here:', function(code) {
-		// request access token
-		oauth2Client.getToken(code, function(err, tokens) {
-		  // set tokens to the client
-		  // TODO: tokens should be set by OAuth2 client.
-		  oauth2Client.setCredentials(tokens);
-		  console.log(tokens);
-		  res.render('auth');
-		});
-	});
-	
+	auth.onLogin( function (err) {
+	  	console.log(err);
+  	});
 });
 
 module.exports = router;
