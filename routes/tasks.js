e3ad61068482780716086ecc94273d4289661841
Danil Nikiforov <danil.nikiphorov@gmail.com>
2014-07-03 19:56:49 +0700

    added connection to psql

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,10 +9,45 @@ router.get('/', function(req, res) {
 	            error: err
         	});
 		else{
+			saveLists(tasks);
 			res.render('tasks', {tasks : tasks});	
 		}
 	});
 });
+
+function saveLists (tasks) {
+	var pg = require('pg');
+	var conString = "postgres://postgres:123@localhost/smart_tasks";
+	var client = new pg.Client(conString);
+	
+	client.connect(function(err) {
+	  if(err) {
+	    return console.error('could not connect to postgres', err);
+	  }else{
+	  	debugger;
+	  	client.query("CREATE TABLE IF NOT EXISTS tasks");
+	  	for (var i = tasks.length - 1; i >= 0; i--) {
+			client.query({
+		  		name: 'insert tasks',
+		  		text: "INSERT INTO tasks(title) values($1)",
+		  		values: [tasks[i].title]
+			});
+		};
+		var query = client.query("SELECT * FROM tasks");
+
+		query.on('row', function(row) {
+		  console.log(row);
+		});
+
+		query.on('end', function() { 
+		  client.end();
+		});
+	  }
+	});
+
+	
+	
+}
 
 
 function fetchTasks(callback){
