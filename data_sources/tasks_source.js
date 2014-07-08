eb755cf1f3641412a6255fa36dcf62b8c393baa1
Danil Nikiforov <danil.nikiphorov@gmail.com>
2014-07-08 13:38:27 +0700

    refactoring. fixed errors with running middlewares

--- data_sources/tasks_source.js
+++ data_sources/tasks_source.js
@@ -1,18 +1,23 @@
 var googleapis = require('googleapis');
 var auth = require('../auth/auth.js');
 
-exports.fetchTasks= function(callback){
-	googleapis
-  		.discover('tasks', 'v1')
-  		.execute(function(err, client) {
-		getUserTasks(client, function(err, lists) {
-	      if (err) {
-	        console.log('An error occured', err);
-	      }
-	      console.log(lists);
-	      callback(lists.items, err);
+module.exports.fetchTasks= function(req, res, next){
+    console.log("start fetchTasks");
+  	googleapis
+    		.discover('tasks', 'v1')
+    		.execute(function(err, client) {
+          console.log("got googleapis client");
+  		getUserTasks(client, function(err, lists) {
+  	      if (err) {
+  	        console.log('An error occured', err);
+            next(err);
+  	      }
+          console.log("!!lists.length");
+  	      console.log(lists.length);
+          req.tasks = lists.items;
+          next();
+      	});
     	});
-  	});
 }
 
 function getUserTasks(client, callback) {
