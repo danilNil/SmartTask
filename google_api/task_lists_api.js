5208cddf4c998343dbc396cfc724272423361409
danilNil <danil.nikiphorov@gmail.com>
2014-12-01 12:27:05 +0600

    fix some errors in packages

--- google_api/task_lists_api.js
+++ google_api/task_lists_api.js
@@ -3,25 +3,21 @@ var auth = require('../auth/auth.js');
 
 module.exports.fetchTasks= function(req, res, next){
     console.log("start fetchTasks");
-  	googleapis
-    		.discover('tasks', 'v1')
-    		.execute(function(err, client) {
-          console.log("got googleapis client");
+  	 var client =googleapis.tasks('v1',auth.oauth2Client());
+     console.log(client);
+     console.log(client.tasks);
   		getUserTasks(client, function(err, lists) {
-  	      if (err) {
-  	        console.log('An error occured', err);
-            next(err);
-  	      }
-          req.tasks = lists.items;
-          next();
-      	});
+	      if (err) {
+	        console.log('An error occured', err);
+          next(err);
+	      }
+        req.tasks = lists.items;
+        next();
     	});
 }
 
 function getUserTasks(client, callback) {
   client
-    .tasks.tasklists.list()
-    .withAuthClient(auth.oauth2Client())
-    .execute(callback);
+    .tasks.list(callback);
 }
 
